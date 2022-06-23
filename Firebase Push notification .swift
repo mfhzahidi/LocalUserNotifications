1a5f415dde3638aa576d2537d1d3ea417ecdf30b
FAHAD HASAN Zahidi <mfhzahidi@gmail.com>
2022-06-23 23:20:41 +0600

    Firebase push notification tutorial text

---
+++ Firebase Push notification .swift
@@ -0,0 +1,18 @@
+//
+//  Firebase Push notification .swift
+//  LocalPushNotifications
+//
+//  Created by Macbook Pro on 23/6/22.
+//  Copyright © 2022 Fahad Hasan Zahidi. All rights reserved.
+//
+
+import Foundation
+
+//Push notifications
+//1. Init pod - 1.firebase/core, 2.firebase/messaging
+//2. Add push notifications capability in signing and certificate
+//3. 1. addDelegate - import firebase, userNotification 2.configure firebase
+//4. Firebase dashboard - 1.create project 2. Register app with bundle id 3.download configure file 4. Add the file to project
+//5. App delegate to register firebase 1. Set delegates to self 2. Request authorisation of user notification 3. Register for remote notifications 4. Add didReceiveRegistrationToken
+//6. Developer site - 1. Create key 2. Enable APNs 3. Download key 4. Add to firebase site
+
